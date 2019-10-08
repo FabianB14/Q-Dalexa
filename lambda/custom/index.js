9a91353d1742edfad59e00133439f0f93227c63d
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-10-08 11:05:07 -0700

    This is to update the dependices

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,6 +1,7 @@
 // This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
 // Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 // session persistence, api calls, and more.
+const aws = require('aws-sdk');
 const Alexa = require('ask-sdk-core');
 
 const LaunchRequestHandler = {
